7e2842eee1a69a2d872dc0fc4375925f147ec17f
unaLoo <huanyu.lu@foxmail.com>
2024-09-20 14:18:43 +0800

    transplate-flow

--- public/shaders/06flow/delaunay.frag.glsl
+++ public/shaders/06flow/delaunay.frag.glsl
@@ -3,15 +3,27 @@ precision highp float;
 
 in vec2 v_velocity;
 
-// uniform sampler2D u_maskTexture;
+uniform sampler2D u_maskTexture;
 
 out vec2 fragColor;
 
 void main() {
-    if(v_velocity == vec2(0.0)){
-        discard;
+
+    vec2 fragCoord = gl_FragCoord.xy;
+    vec2 screenSize = vec2(textureSize(u_maskTexture, 0));
+    vec2 uv = fragCoord / screenSize;
+    float valid = texture(u_maskTexture, uv).r;
+
+    if(valid == 0.0f) {
+        fragColor = vec2(0.0f);
+        return;
     }
+
     fragColor = v_velocity;
-    // fragColor = vec4(v_velocity, 0.0f, 1.0f);
+
+    // if(v_velocity == vec2(0.0f)) {
+    //     discard;
+    // }
+    // fragColor = v_velocity;
     // fragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@ import mapbox from 'mapbox-gl';
 // import { main, main3d } from '../exps/03transform/main'
 // import { initMap } from '../exps/04polygon/main'
 // import { main } from '../exps/05XFBO/main'
-import { initMap } from '../exps/06flow/main'
+import { initMap } from '../exps/06flow/mainWithMask'
 // import { initMap, EulerFlowLayer } from '../exps/07arrow/main'
 // import { initMap } from '../exps/07arrow/withMask'
 // import { main } from '../exps/培训案例/pulsing';

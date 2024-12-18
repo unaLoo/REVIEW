d309a4d2362b76c27e91916b7d05ad0d625b6263
Nujabes <1923606858@qq.com>
2024-12-18 15:19:36 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
-// import { play } from '../WebGPU/enter.js'
-import { initMap } from "../exps/04PointNoParkinsons/main.ts"
+import { play } from '../WebGPU/enter.js'
+// import { initMap } from "../exps/04PointNoParkinsons/main.ts"
 
 
 onMounted(() => {
 
-  initMap()
-  // play()
+  // initMap()
+  play()
 
 })
 
@@ -16,8 +16,8 @@ onMounted(() => {
 <template>
   <div class="main">
 
-    <!-- <canvas id='playground'></canvas> -->
-    <div id="map"></div>
+    <canvas id='playground'></canvas>
+    <!-- <div id="map"></div> -->
   </div>
 </template>
 
